feb37e1684dc4a18ce9517a5afab0809dd00b660
asqarbekolimov <mr.coderx.pro@gmail.com>
2022-08-06 10:39:04 +0500

    Vanilla JS animation and loader

--- script/app.js
+++ script/app.js
@@ -1,6 +1,52 @@
-const hamburger = document.querySelector(".menu-btn img"),
-  nav = document.querySelector(".navigation");
+window.addEventListener("DOMContentLoaded", () => {
+  const hamburger = document.querySelector(".menu-btn img"),
+    nav = document.querySelector(".navigation"),
+    header = document.querySelector("header"),
+    navigationItems = document.querySelectorAll(".navigation a");
+  const scrollBtn = document.querySelector(".scrollTotopBtn");
+  const loader = document.querySelector(".loader");
 
-hamburger.addEventListener("click", () => {
-  nav.classList.toggle("active");
+  setTimeout(() => {
+    loader.style.opacity = "0";
+    setTimeout(() => {
+      loader.style.display = "none";
+    }, 500);
+  }, 1500);
+
+  window.addEventListener("scroll", () => {
+    header.classList.toggle("sticky", window.scrollY > 5);
+  });
+
+  hamburger.addEventListener("click", () => {
+    nav.classList.toggle("active");
+  });
+
+  navigationItems.forEach((navItem) => {
+    navItem.addEventListener("click", () => {
+      nav.classList.remove("active");
+    });
+  });
+
+  window.addEventListener("scroll", () => {
+    scrollBtn.classList.toggle("active", window.scrollY > 500);
+  });
+
+  scrollBtn.addEventListener("click", () => {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+  });
+
+  const reveals = document.querySelectorAll(".reveal");
+
+  window.addEventListener("scroll", () => {
+    for (let i = 0; i < reveals.length; i++) {
+      let windowHeight = window.innerHeight;
+      let revealTop = reveals[i].getBoundingClientRect().top;
+      let revealPoint = 50;
+
+      if (revealTop < windowHeight - revealPoint) {
+        reveals[i].classList.add("active");
+      }
+    }
+  });
 });
